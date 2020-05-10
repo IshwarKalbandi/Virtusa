d2df5611cdbeedd70670f977a64e663c04c7e27d
IshwarKalbandi <65116173+IshwarKalbandi@users.noreply.github.com>
2020-05-10 17:35:05 +0530

    Add files via upload

---
+++ Sample/src/main/java/org/dypiemr/Sample/Appdyp.java
@@ -0,0 +1,16 @@
+package org.dypiemr.Sample;
+
+import org.dypiemr.Samplecalculator.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class Appdyp 
+{
+    public static void main( String[] args )
+    {
+    	double result= App.add(5,6);
+        System.out.println( "Hello calculator add  "+result );
+    }
+}
